c7e1a06a4080bdda5c488d966620e65a26982d43
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-25 15:21:49 +0300

    Added django debug toolbar

--- library_team_project/settings.py
+++ library_team_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "rest_framework",
     "payment",
     "borrowing",
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,3 +131,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "user.User"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- library_team_project/urls.py
+++ library_team_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/book/", include("book.urls", namespace="book"))
+    path("api/book/", include("book.urls", namespace="book")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
