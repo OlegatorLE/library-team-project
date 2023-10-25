8ea2238c5ce5a97318d7372b43589f52239cb804
Roman Podolets <romanpyu@gmail.com>
2023-10-25 14:26:19 +0300

    Added payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+from borrowing.models import Borrowing
+
+
+class Payment(models.Model):
+    STATUS_CHOICES = (
+        (0, "PENDING"),
+        (1, "PAID")
+    )
+
+    TYPE_CHOICES = (
+        (0, "PAYMENT"),
+        (1, "FINE")
+    )
+
+    status = models.IntegerField(
+        choices=STATUS_CHOICES,default=0
+    )
+    type = models.IntegerField(
+        choices=TYPE_CHOICES, default=0
+    )
+
+    borrowing_id = models.ForeignKey(
+        Borrowing, on_delete=models.CASCADE, related_name="payments"
+    )
+
+    session_url = models.CharField(max_length=255)
+    session_id = models.CharField(max_length=63)
+
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Payment: {self.id}; Pay: {self.money_to_pay};"
