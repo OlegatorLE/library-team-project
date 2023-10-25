7602075e79c99edf6450637799091a9fcb0b44bf
Naholiuk Yevhen <e.nagolyuk@gmail.com>
2023-10-25 12:54:07 +0300

    Corrected settings.py

--- library_team_project/settings.py
+++ library_team_project/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "borrowing",
     "book",
     "payment",
-    "borrowing",
     "user",
+    # fsdf
 ]
 
 MIDDLEWARE = [
