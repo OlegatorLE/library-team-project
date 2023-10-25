4e5a27b945f8c6282f096b39cbf5e96b97c93513
9rosLove <yarik2300@gmail.com>
2023-10-25 16:44:04 +0300

    Refactored serializers.py

--- payment/serializers.py
+++ payment/serializers.py
@@ -4,17 +4,13 @@ from payment.models import Payment
 
 
 class PaymentSerializer(serializers.ModelSerializer):
-    status = serializers.IntegerField(read_only=True)
-    type = serializers.IntegerField(read_only=True)
-    session_url = serializers.CharField(read_only=True)
-    session_id = serializers.CharField(read_only=True)
-
     class Meta:
         model = Payment
         fields = "__all__"
+        read_only_fields = ("status", "type", "session_url", "session_id")
 
 
-class PaymentListSerializer(PaymentSerializer):
+class PaymentListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = ("id", "status", "type", "money_to_pay")
