1b088c6c8cf47de8eee6fa538dd15c4f04cd4304
Kristian Yendrek <kristianendreck4@gmail.com>
2023-10-25 16:06:39 +0300

    created BorrowingViewSet and List/DetailSerializers

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
-urlpatterns = [
+from borrowing.views import BorrowingViewSet
 
-]
+router = routers.DefaultRouter()
+router.register("borrowings", BorrowingViewSet)
+
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "borrowing"
--- borrowing/views.py
+++ borrowing/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+from borrowing.models import Borrowing
+from borrowing.serializers import BorrowingSerializer, BorrowingListSerializer
+
 
 # Create your views here.
+class BorrowingViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet,
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset.select_related("book", "user")
+
+        user_id = self.request.query_params.get("user_id")
+        is_active = self.request.query_params.get("is_active")
+
+        if user_id:
+            queryset = queryset.filter(user_id=int(user_id))
+
+        if is_active:
+            is_active = is_active.lower()
+
+            if is_active == "false":
+                queryset = queryset.filter(actual_return_date__isnull=False)
+
+            if is_active == "true":
+                queryset = queryset.filter(actual_return_date__isnull=True)
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        return self.serializer_class
