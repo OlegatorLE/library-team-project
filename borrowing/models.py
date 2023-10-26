b57a1aec380f3785e2252708dbc889592bd13c77
Roman Podolets <romanpyu@gmail.com>
2023-10-26 12:48:11 +0300

    Added transaction to create borrowing & overdue property to borrowing model

--- borrowing/models.py
+++ borrowing/models.py
@@ -25,5 +25,14 @@ class Borrowing(models.Model):
             (self.borrow_date - self.expected_return_date).days
         )
 
+    @property
+    def overdue(self):
+        if self.actual_return_date:
+            return (
+                (self.actual_return_date - self.expected_return_date).days *
+                self.book.daily_fee
+            )
+        return 0
+
     def __str__(self):
         return f"{self.book} ({self.user})"
