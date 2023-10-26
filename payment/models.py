9fb721cd29db5aba3b832db67daf02e7821813f7
Roman Podolets <romanpyu@gmail.com>
2023-10-26 11:33:34 +0300

    Changed in Payment session_url attr max_lenght to 512 chars

--- payment/models.py
+++ payment/models.py
@@ -25,7 +25,7 @@ class Payment(models.Model):
         Borrowing, on_delete=models.CASCADE, related_name="payments"
     )
 
-    session_url = models.CharField(max_length=255)
+    session_url = models.CharField(max_length=512)
     session_id = models.CharField(max_length=63)
 
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
