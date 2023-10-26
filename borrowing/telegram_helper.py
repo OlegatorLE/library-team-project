c42b463f8b9a55a63429f419d7a46263e16802b4
OlegatorLE <oleh.travin@gmail.com>
2023-10-26 18:25:15 +0300

    Implemented pyTelegramBotAPI

--- borrowing/telegram_helper.py
+++ borrowing/telegram_helper.py
@@ -1,9 +1,9 @@
-import telegram
-import asyncio
+import telebot
+
 from django.conf import settings
 
+bot = telebot.TeleBot(settings.TELEGRAM_API_KEY)
 
-async def send_notification(message_text, chat_id):
-    bot = telegram.Bot(token=settings.TELEGRAM_API_KEY)
-    async with bot:
-        await bot.send_message(chat_id=chat_id, text=message_text)
+
+def send_notification(message_text, chat_id):
+    bot.send_message(chat_id=chat_id, text=message_text)
